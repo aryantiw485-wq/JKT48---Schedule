f08b7ffb3de53b29f7713a147a6fe76efda299b6
aryantiw485-wq <aryantiw485@gmail.com>
2026-06-23 13:38:23 +0700

    Test Nitter

--- main.py
+++ main.py
@@ -1,38 +1,14 @@
-import os
+import requests
 
-print("DEBUG START")
-print(os.environ.keys())
-print("DEBUG END")
+url = "https://nitter.net/moshi2jkt48"
 
-import os
-import json
-from datetime import datetime, timedelta
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-
-creds_json = json.loads(os.environ["GOOGLE_CREDENTIALS"])
-
-credentials = service_account.Credentials.from_service_account_info(
-    creds_json,
-    scopes=["https://www.googleapis.com/auth/calendar"]
+r = requests.get(
+    url,
+    headers={
+        "User-Agent": "Mozilla/5.0"
+    }
 )
 
-service = build("calendar", "v3", credentials=credentials)
-
-calendar_id = os.environ["CALENDAR_ID_THEATER"]
-
-event = {
-    "summary": "TEST EVENT JKT48 BOT",
-    "start": {
-        "dateTime": (datetime.utcnow() + timedelta(minutes=1)).isoformat() + "Z",
-        "timeZone": "Asia/Jakarta",
-    },
-    "end": {
-        "dateTime": (datetime.utcnow() + timedelta(minutes=61)).isoformat() + "Z",
-        "timeZone": "Asia/Jakarta",
-    },
-}
-
-service.events().insert(calendarId=calendar_id, body=event).execute()
-
-print("TEST EVENT BERHASIL DIBUAT 🫡")
+print("STATUS:", r.status_code)
+print("PANJANG HALAMAN:", len(r.text))
+print(r.text[:500])
