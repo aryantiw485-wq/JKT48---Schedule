fdfd1cacea86ecf0eca45e3c5bdbafb5c231dc31
aryantiw485-wq <aryantiw485@gmail.com>
2026-06-23 15:36:52 +0700

    persing 1

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import xml.etree.ElementTree as ET
 import re
 
 
+TARGET_MEMBER = [
+    "Gita",
+    "Gita Sekar Andarini"
+]
+
+
 url = "https://nitter.net/moshi2jkt48/rss"
 
 headers = {
-    "User-Agent": "Mozilla/5.0",
-    "Accept": "application/rss+xml,application/xml,text/xml,*/*"
+    "User-Agent": "Mozilla/5.0"
 }
 
 
@@ -28,60 +33,39 @@ for item in items:
     text = title.text
 
 
-    # hanya ambil tweet daftar member
     if "Berikut adalah member yang akan tampil" not in text:
         continue
 
 
-    print("\n======================")
-    print("DATA DITEMUKAN")
-    print("======================")
+    # Ambil bagian setelah WIB (daftar member)
+    member_list = text.split("WIB")[-1]
 
 
-    # Ambil nama show
-    show = re.search(
-        r"pertunjukan (.*?) \|",
-        text
+    ada_gita = any(
+        nama in member_list
+        for nama in TARGET_MEMBER
     )
 
-    if show:
-        nama_show = show.group(1)
-    else:
-        nama_show = "Tidak diketahui"
+
+    if ada_gita:
+
+        show = re.search(
+            r"pertunjukan (.*?) \|",
+            text
+        )
+
+        tanggal = re.search(
+            r"\| (\d{1,2} \w+ \d{4}) \|",
+            text
+        )
+
+        jam = re.search(
+            r"\| (\d{2}\.\d{2}) WIB",
+            text
+        )
 
 
-    # Ambil tanggal
-    tanggal = re.search(
-        r"\| (\d{1,2} \w+ \d{4}) \|",
-        text
-    )
-
-    if tanggal:
-        tanggal_show = tanggal.group(1)
-    else:
-        tanggal_show = "Tidak diketahui"
-
-
-    # Ambil jam
-    jam = re.search(
-        r"\| (\d{2}\.\d{2}) WIB",
-        text
-    )
-
-    if jam:
-        jam_show = jam.group(1)
-    else:
-        jam_show = "Tidak diketahui"
-
-
-    # Cek Gita
-    if "Gita" in text:
-        status = "⭐ GITA TAMPIL"
-    else:
-        status = "Bukan Gita"
-
-
-    print("Show   :", nama_show)
-    print("Tanggal:", tanggal_show)
-    print("Jam    :", jam_show)
-    print("Status :", status)
+        print("⭐ GITA TAMPIL ⭐")
+        print("Show   :", show.group(1))
+        print("Tanggal:", tanggal.group(1))
+        print("Jam    :", jam.group(1))
