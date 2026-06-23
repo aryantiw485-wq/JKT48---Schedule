cc5be5cbf8a02e269b545a06df5afb7219822890
aryantiw485-wq <aryantiw485@gmail.com>
2026-06-23 15:03:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ root = ET.fromstring(r.text)
 
 items = root.findall(".//item")
 
-ditemukan = False
+gita_ditemukan = False
 
 for item in items:
     title = item.find("title")
@@ -24,7 +24,16 @@ for item in items:
 
     text = title.text
 
-    if "Berikut adalah member yang akan tampil" in text:
-        print()
-        print("=" * 80)
+    if (
+        "Berikut adalah member yang akan tampil" in text
+        and "Gita" in text
+    ):
+        gita_ditemukan = True
+
+        print("⭐ GITA DITEMUKAN ⭐")
         print(text)
+
+if gita_ditemukan:
+    print("AKAN MEMBUAT EVENT KALENDER")
+else:
+    print("Belum ada show Gita.")
