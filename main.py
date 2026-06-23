dc324479147fe3f6dd7e8279683dc5da916d4db1
aryantiw485-wq <aryantiw485@gmail.com>
2026-06-23 14:28:26 +0700

    RSS Test

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
 
-r = requests.get("https://example.com")
+url = "https://nitter.net/moshi2jkt48/rss"
+
+r = requests.get(url)
 
 print("STATUS:", r.status_code)
 print("PANJANG:", len(r.text))
+
+print(r.text[:1000])
