ccc01a19c79f46efd33a29ecfbad06e3a5513719
aryantiw485-wq <aryantiw485@gmail.com>
2026-06-25 10:18:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -82,6 +82,8 @@ Berikut adalah member yang akan tampil pada pertunjukan Sambil Menggandeng Erat 
 
         text = title.text or ""
 
+        print("RSS:", text)
+        
         if "Berikut adalah member yang akan tampil" not in text:
             continue
 
